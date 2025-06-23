523f4355bcfee53c438492475c42ba996a05e5d0
Miguel Figueroa <miguelfigueroa90@gmail.com>
2025-06-23 09:09:29 -0400

    Adding temperatur convertion operations

--- temperature.php
+++ temperature.php
@@ -1,3 +1,34 @@
+<?php
+    $value = filter_input(type: INPUT_POST, var_name: 'value');
+    $from = filter_input(type: INPUT_POST, var_name: 'from');
+    $to = filter_input(type: INPUT_POST, var_name: 'to');
+    $result = null;
+    switch ($from) {
+        case 'c':
+            $result = match ($to) {
+                'c' => $value,
+                'f' => $value * (9/5) + 32,
+                'k' => $value + 273.15
+            };
+            break;
+
+        case 'f':
+            $result = match ($to) {
+                'c' => ($value -32) * (5/9),
+                'f' => $value,
+                'k' => ($value -32) * (5/9) + 273.15
+            };
+            break;
+
+        case 'k':
+            $result = match ($to) {
+                'c' => $value - 273.15,
+                'f' => ($value - 273.15) * (9/5) + 32,
+                'k' => $value
+            };
+            break;
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,36 +55,42 @@
             </ul>
         </div>
 
-        <div>
-            <form action="temperature.php" method="post">
-                <div class="my-4">
-                    <label for="temperature" class="block mb-2 text-sm font-medium text-gray-900">Enter the temperature to convert</label>
-                    <input type="text" id="temperature" name="temperature" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
-                </div>
+        <?php if ($result) : ?>
+            <h2 class="text-xl font-semibold block my-4">Result of your calculation</h2>
+            <span class="block text-2xl font-bold my-4"><?= sprintf("%d %s = %f %s", $value, $from, $result, $to); ?></span>
+            <a href="temperature.php" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Reset</a>
+        <?php else: ?>
+            <div>
+                <form action="temperature.php" method="post">
+                    <div class="my-4">
+                        <label for="value" class="block mb-2 text-sm font-medium text-gray-900">Enter the temperature to convert</label>
+                        <input type="text" id="value" name="value" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+                    </div>
 
-                <div class="my-4">
-                    <label for="from" class="block mb-2 text-sm font-medium text-gray-900">Unit to convert from</label>
-                    <select id="from" name="from" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
-                        <option selected></option>
-                        <option value="c">Celsius</option>
-                        <option value="f">Fahrenheit</option>
-                        <option value="k">Kelvin</option>
-                    </select>
-                </div>
+                    <div class="my-4">
+                        <label for="from" class="block mb-2 text-sm font-medium text-gray-900">Unit to convert from</label>
+                        <select id="from" name="from" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
+                            <option selected></option>
+                            <option value="c">Celsius</option>
+                            <option value="f">Fahrenheit</option>
+                            <option value="k">Kelvin</option>
+                        </select>
+                    </div>
 
-                <div class="my-4">
-                    <label for="to" class="block mb-2 text-sm font-medium text-gray-900">Unit to convert to</label>
-                    <select id="to" name="to" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
-                        <option selected></option>
-                        <option value="c">Celsius</option>
-                        <option value="f">Fahrenheit</option>
-                        <option value="k">Kelvin</option>
-                    </select>
-                </div>
+                    <div class="my-4">
+                        <label for="to" class="block mb-2 text-sm font-medium text-gray-900">Unit to convert to</label>
+                        <select id="to" name="to" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
+                            <option selected></option>
+                            <option value="c">Celsius</option>
+                            <option value="f">Fahrenheit</option>
+                            <option value="k">Kelvin</option>
+                        </select>
+                    </div>
 
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Convert</button>
-            </form>
-        </div>
+                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Convert</button>
+                </form>
+            </div>
+        <?php endif ?>
     </div>
 </body>
 </html>
